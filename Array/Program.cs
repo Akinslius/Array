263e2530454bda855c3498f30feef505066d03f5
Akinslius <akinyemi.akinwande@yahoo.com>
2022-06-24 11:28:11 +0100

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] scores = { 1, 4, 7, 5 };
+            //for (int i = 0; i < scores.Length; i++)
+            //{
+            //    Console.Write(scores[i]);
+            //}
+            //foreach (int k in scores)
+            //{
+            //    Console.WriteLine(k);
+            //}
+
+            //Console.WriteLine("Hello World!");
+            string[] Names = { "James", "John", "Micheal" };
+            for (int i = 0; i < Names.Length; i++)
+            {
+                Console.WriteLine(Names[i]);
+            }
+
+            Console.WriteLine("Using foreach loop");
+
+            foreach (string name in Names)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
